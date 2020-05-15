986257cfd69b699e8d4b19794f4dae00e0adc526
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2020-05-15 10:39:41 +0200

    attempt of upload files to heroku

--- server/app.js
+++ server/app.js
@@ -17,10 +17,9 @@ require('./configs/views.configs')(app)
 require('./configs/locals.config')(app)
 
 // Base URLS
-app.use((req, res) => {
-    res.sendFile(__dirname + "/public/index.html")
-})
-app.use("/api", require("/routes"))
+// app.use("/api",require("/routes"))
+
+
 
 app.use('/api', require('./routes/auth.routes'))
 app.use('/api/games', require('./routes/games.routes'))
@@ -28,4 +27,7 @@ app.use('/api/events', require('./routes/events.routes'))
 app.use('/api/users', require('./routes/users.routes'))
 app.use('/api/files', require('./routes/files.routes'))
 
+// app.use((req,res)=>{
+// res.sendFile(__dirname+"/public/index.html");
+// })
 module.exports = app
--- server/configs/views.configs.js
+++ server/configs/views.configs.js
@@ -1,4 +1,4 @@
-const favicon = require('serve-favicon')
+// const favicon = require('serve-favicon')
 const path = require('path')
 const express = require('express')
 
@@ -6,5 +6,5 @@ module.exports = app => {
     app.set('views', path.join(__dirname, '..', 'views'))
     app.set('view engine', 'hbs')
     app.use(express.static(path.join(__dirname, '..', 'public')))
-    app.use(favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.ico')))
+    // app.use(favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.ico')))
 }
